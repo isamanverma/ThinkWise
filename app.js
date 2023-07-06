1f41e3104dd4c51a2ffdfc3b23ad6a29f256571e
amankumarverma <theamankumarverma@gmail.com>
2023-07-06 19:00:40 +0530

    fixed the image change

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function updateBackgroundImage() {
 }
 
 updateBackgroundImage();
-setInterval(updateBackgroundImage, 5000);
+setInterval(updateBackgroundImage, 60000);
 
 
 
@@ -23,29 +23,36 @@ function updateQuote() {
     const thoughtElement = document.querySelector('.thought');
     const authorElement = document.querySelector('.author');
     const categoryElement = document.querySelector('.category');
-
-    fetch('https://api.api-ninjas.com/v1/quotes', {
-        headers: {
-            'X-Api-Key': 'XvWeWkc4YJ1+6QXRWoTB3Q==hboGMuzmpu9XDBNM'
-        }
+  
+    const categories = ['success', 'money', 'life', 'faith', 'failure', 'communications', 'humor'];
+    const category = categories[Math.floor(Math.random() * categories.length)];
+  
+    fetch('https://api.api-ninjas.com/v1/quotes?category=' + category, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': 'YGt5VMtbCqFOC2TLzH3/kg==rl6owKbAWCvwhkyF',
+        'Content-Type': 'application/json'
+      }
     })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error(response.status);
-            }
-        })
-        .then(data => {
-            const quote = data[0];
-            thoughtElement.textContent = quote.quote;
-            authorElement.textContent = quote.author;
-            categoryElement.textContent = quote.category;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-updateQuote();
-setInterval(updateQuote, 5025);
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.status);
+        }
+      })
+      .then(data => {
+        const quote = data[0];
+        thoughtElement.textContent = quote.quote;
+        authorElement.textContent = quote.author;
+        categoryElement.textContent = quote.category;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  
+  }
+  
+  updateQuote();
+  setInterval(updateQuote, 60001);
+  
